4df9dd10340f4c05d409c605e3125564cbc94b9c
RyanBrewer317 <ryanbrew317@gmail.com>
2021-08-08 22:33:20 -0700

    add a relational sqlite database and an ebiten game window

--- pkg/go.mod
+++ pkg/go.mod
@@ -2,4 +2,7 @@ module rbrewer.com/westpunk
 
 go 1.16
 
-require github.com/hajimehoshi/ebiten/v2 v2.1.4 // indirect
+require (
+	github.com/hajimehoshi/ebiten/v2 v2.1.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
+)
--- pkg/westpunk.go
+++ pkg/westpunk.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// go-sqlite3 docs https://github.com/mattn/go-sqlite3/blob/v1.14.8/_example/simple/simple.go
+// ebiten docs https://ebiten.org/tour/hello_world.html
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	_, err = db.Exec(`create table places (placeID text not null primary key, location text not null);`)
+	if err != nil {
+		log.Printf("%q: %s\n", err, `create table places (placeID text not null primary key, location text not null`)
+		return
+	}
+
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }

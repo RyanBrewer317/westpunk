ab2717e79f895a91f86c10046c77f97cb566af6d
Ryan Brewer <ryanbrew317@gmail.com>
2021-10-07 11:58:55 -0700

    update linux support for python build scripts- macos and windows should be tested next

--- pkg/westpunk/build_core.py
+++ pkg/westpunk/build_core.py
@@ -3,20 +3,23 @@ from zipfile import ZipFile
 from pathlib import Path
 from shutil import copy
 from os import remove, environ
-from os.path import join
+from os.path import join, split, sep
 from sys import argv
 
 root = Path(__file__).parent.parent.parent.as_uri().replace("file:///", "")
+if root[0] != sep and root[1] != ':':
+    root = sep + root
 
 def build(goos, goarch, exe):
+    filename = split(exe)[1]
     with ZipFile(f"westpunk_{goos}.zip", "w") as zip:
         environ["GOOS"] = goos
         environ["GOARCH"] = goarch
-        run("go install")
+        run(["go", "install"])
         exepath = join(root, "bin", exe)
         thispath = exepath.replace(join("bin", exe), join("pkg", "westpunk"))
-        copy(exepath, join(thispath, exe))
-        zip.write(join(thispath, exe), arcname=exe)
+        copy(exepath, join(thispath, filename))
+        zip.write(join(thispath, filename), arcname=filename)
         zip.write(join(thispath, "background.png"), arcname="background.png")
         zip.write(join(thispath, "database.db"), arcname="database.db")
         zip.write(join(thispath, "dirtlayerground.png"), arcname="dirtlayerground.png")
@@ -24,7 +27,7 @@ def build(goos, goarch, exe):
         zip.write(join(thispath, "oaklog.png"), arcname="oaklog.png")
         zip.write(join(thispath, "spritesheet.png"), arcname="spritesheet.png")
         zip.write(join(thispath, "tree.png"), arcname="tree.png")
-        remove(join(thispath, exe))
+        remove(join(thispath, filename))
 
 if __name__ == "__main__" and len(argv) > 1:
     build(argv[1], argv[2], argv[3])
--- pkg/westpunk/full_linux_version.py
+++ pkg/westpunk/full_linux_version.py
@@ -1,7 +1,8 @@
 import build_core
+from os.path import join
 
 def linux():
-    build_core.build("linux", "amd64", "westpunk")
+    build_core.build("linux", "amd64", join("linux", "bin", "westpunk"))
 
 if __name__ == "__main__":
     # linux() # THIS OVERWRITES THE MACOS EXECUTABLE SO SHOULD ONLY BE CALLED AS A PART OF BUILD.PY
